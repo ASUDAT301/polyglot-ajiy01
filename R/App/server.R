c9658ad66a11ebe27b14e13e74dc744c50b9a343
Ajay Iyer <ajayiyer@Ajays-MacBook-Pro.local>
2021-12-02 18:14:10 -0700

    Server Code

--- R/App/server.R
+++ R/App/server.R
@@ -15,19 +15,23 @@ shinyServer(function(input, output,session) {
     
     bb = read.csv("data/bb_data.csv")
     
+    #User Input on Team Selected
     data <- reactive({
       req(input$team)
       df <- bb %>% filter(Tm %in% input$team) %>% group_by(Age) %>% summarise(FG = sum(FG))
     })
     
+    #Team select
     observe({
       updateSelectInput(session, "team", choices = bb$Tm)
     })
     
+    #Plot FG vs Age
     output$Age_FG <- renderPlot({
       ggplot(data(), aes(y = FG, x=Age)) + geom_bar(stat="sum", color="black", fill="blue") + ggtitle("Average Field Goals by Age")
     })
     
+    #Download Button
     output$down <- downloadHandler(
       filename=function(){
         paste("bbgraph", input$dload, sep=".")
